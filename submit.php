2fb01d6f2edf6995f0fb3aba1f71564b82d96f12
master <github@email.ua>
2018-07-09 23:11:15 +0300

    1rst addmstks

--- submit.php
+++ submit.php
@@ -3,15 +3,17 @@ error_reporting(E_ALL);
 ini_set('display_errors', '1');
 require_once('Repository.php');
 
-if (isset($_POST['username']) & $_POST['username'] !== "")
+if (isset($_POST['username']) & $_POST['username'] !== " ")
 {
     $username = htmlspecialchars(trim($_POST['username']));
 }
 
-if (isset($_POST['message']) && $_POST['message'] !== "")
+if (isset($_POST['message']) && $_POST['message'] !== " ")
 {
-    $message =  htmlspecialchars(trim($_POST['message']));
+    $message =  htmlspecialchars($_POST['message']);
 
+}else{
+    die("Wrong data");
 }
 
 $repo = new Repository();
